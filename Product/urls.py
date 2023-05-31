52930a2d8073a2c4dea449ce98786bc487a8cc6b
Lale004 <qocayevalale38@gmail.com>
2023-05-31 21:23:05 +0400

    son

--- Product/urls.py
+++ Product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('',views.home),
+    path('',views.home,name='home'),
     path('products',views.product_list,name='products'),
     path('product/<int:id>',views.product,name='product' ),
     path('product_create',views.product_create,name='product_create')
